9d3064adfccaf9ddb6aad3e4b7a86422a8ebed3d
fabQ <fabien.quinquis@gmail.com>
2021-01-21 11:15:37 +0100

    Docstring generation

--- R/simulation.R
+++ R/simulation.R
@@ -7,6 +7,9 @@
 #' @export
 #'
 #' @examples simu_A(n = 30, alpha = c(1.5, 4.5, 1, 3))
+
+
 simu_A = function(n, alpha = c(1.5, 4.5, 1, 3)){
   tmp_mix_Lvar = t(gtools::rdirichlet(n = n, alpha = alpha))
 }
+
